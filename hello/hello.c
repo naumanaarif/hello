25eba040f5ee55a9158a4f80b4f8b7ea742a3fa7
Nauman Arif <naumanaarif.dev@gmail.com>
2022-09-05 18:30:52 +0500

    Updates hello.c

--- hello/hello.c
+++ hello/hello.c
@@ -2,19 +2,6 @@
 
 int main(void)
 {
-    char name[64];
-
-    printf_s("What's your name? ");
-    scanf_s("%[^\n]s", name);
-
-    if (name[0] != '\0')
-    {
-        printf_s("Hello, %s!", name);
-    }
-    else
-    {
-        printf_s("Hello, world!\n");
-    }
-
+    printf_s("Hello, world!\n");
     return 0;
 }
